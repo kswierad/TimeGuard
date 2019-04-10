b484f99d32075980937de06fb33abe99e16dd75c
kradon <konradradon@gmail.com>
2019-04-10 21:05:40 +0200

    Implementation of time counting classes

--- src/main/java/pl/agh/cs/io/Rule.java
+++ src/main/java/pl/agh/cs/io/Rule.java
@@ -1,11 +1,26 @@
 package pl.agh.cs.io;
 
-import pl.agh.cs.io.State;
-
 public class Rule {
-    String path;
-    
-    public void handle(State State){
-        
+    private String exePath;
+    private Time time;
+
+    public Rule(String path){
+        this.exePath = path;
+        this.time = new Time();
+    }
+
+    public void handle(State state) {
+        switch (state){
+            case FG:
+                time.addFgTime();
+                break;
+            case BG:
+                time.addBgTime();
+                break;
+        }
+    }
+
+    public String getExePath() {
+        return exePath;
     }
 }
--- src/main/java/pl/agh/cs/io/Time.java
+++ src/main/java/pl/agh/cs/io/Time.java
@@ -1,5 +1,27 @@
 package pl.agh.cs.io;
 
 public class Time {
-    int fg, bg;
+    private int fg, bg;
+
+    public Time() {
+        this.fg = 0;
+        this.bg = 0;
+    }
+
+    public void addFgTime() {
+        this.fg += 1;
+        this.bg += 1;
+    }
+
+    public void addBgTime() {
+        this.bg += 1;
+    }
+
+    public int getBg() {
+        return bg;
+    }
+
+    public int getFg() {
+        return fg;
+    }
 }
