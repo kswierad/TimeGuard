93e0538d719fd7f6ec8d8ac4b43cda2703be37d1
ptuduj <paulina.tuduj@gmail.com>
2019-05-30 20:13:50 +0200

    Added warning alert

--- src/main/java/pl/agh/cs/io/api/windows/WindowsListenerRunner.java
+++ src/main/java/pl/agh/cs/io/api/windows/WindowsListenerRunner.java
@@ -1,5 +1,9 @@
 package pl.agh.cs.io.api.windows;
 
+import javafx.application.Platform;
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
+import javafx.util.Duration;
 import pl.agh.cs.io.api.PathProcessId;
 import pl.agh.cs.io.api.ProcessIdsPerPath;
 import pl.agh.cs.io.api.WindowsApi;
@@ -9,11 +13,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 public class WindowsListenerRunner {
-    private final static long POLLING_DELAY_MILLIS = 1000;
+    private final static long POLLING_DELAY_MILLIS = 1;
     private static Consumer<OpenWindowsProcessesPerExeSnapshot> callback;
     private ScheduledExecutorService executorService;
 
@@ -22,25 +25,37 @@ public class WindowsListenerRunner {
     }
 
     public void run(Consumer<OpenWindowsProcessesPerExeSnapshot> callback) {
-        WindowsListenerRunner.callback = callback;
-        executorService.scheduleAtFixedRate(newGetOpenWindowsProcessesTask(),
-                POLLING_DELAY_MILLIS, POLLING_DELAY_MILLIS,
-                TimeUnit.MILLISECONDS);
-    }
 
-    private static Runnable newGetOpenWindowsProcessesTask() {
-        return () -> {
-            PathProcessId foregroundWindow = WindowsApi.getForegroundWindowPathProcessId();
-            List<PathProcessId> windows = WindowsApi.getOpenWindowsPathProcessIds();
-            Map<String, ProcessIdsPerPath> exeWindows = toWindowsPerExeMap(windows);
-            ProcessIdsPerPath foregroundWindowPerExe = exeWindows.remove(foregroundWindow.getPath());
-            if (foregroundWindowPerExe == null) {
-                foregroundWindowPerExe = ProcessIdsPerPath.fromWindow(foregroundWindow);
+        ScheduledService<Void> scheduledService = new ScheduledService<Void>() {
+
+            @Override
+            protected Task<Void> createTask() {
+                return new Task<Void>() {
+                    @Override
+                    protected Void call() throws Exception {
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                PathProcessId foregroundWindow = WindowsApi.getForegroundWindowPathProcessId();
+                                List<PathProcessId> windows = WindowsApi.getOpenWindowsPathProcessIds();
+                                Map<String, ProcessIdsPerPath> exeWindows = toWindowsPerExeMap(windows);
+                                ProcessIdsPerPath foregroundWindowPerExe = exeWindows.remove(foregroundWindow.getPath());
+                                if (foregroundWindowPerExe == null) {
+                                    foregroundWindowPerExe = ProcessIdsPerPath.fromWindow(foregroundWindow);
+                                }
+                                OpenWindowsProcessesPerExeSnapshot snapshot =
+                                        new OpenWindowsProcessesPerExeSnapshot(foregroundWindowPerExe, exeWindows);
+                                callback.accept(snapshot);
+                            }
+                        });
+                        return null;
+                    }
+                };
             }
-            OpenWindowsProcessesPerExeSnapshot snapshot =
-                    new OpenWindowsProcessesPerExeSnapshot(foregroundWindowPerExe, exeWindows);
-            callback.accept(snapshot);
         };
+
+        scheduledService.setPeriod(Duration.seconds(POLLING_DELAY_MILLIS));
+        scheduledService.start();
     }
 
     private static Map<String, ProcessIdsPerPath> toWindowsPerExeMap(List<PathProcessId> windows) {
--- src/main/java/pl/agh/cs/io/model/RuleRestriction.java
+++ src/main/java/pl/agh/cs/io/model/RuleRestriction.java
@@ -1,5 +1,6 @@
 package pl.agh.cs.io.model;
 
+import javafx.scene.control.Alert;
 import pl.agh.cs.io.ExceededUsageAction;
 
 import java.sql.Timestamp;
@@ -15,6 +16,7 @@ public class RuleRestriction {
 
     private long lastNotification;
     private long numOfSecBetweenNotifications = 5;
+    private boolean isDisplayed = false;
 
 
     public RuleRestriction(WindowState state, long permittedNumSec, ExceededUsageAction action) {
@@ -25,20 +27,32 @@ public class RuleRestriction {
     }
 
     public void checkRestriction() {
-        if (rule != null && permittedNumSec > 0) {
-            double usedToday = ActivityTime.getActivityTimeFromList(rule.getTimes(), state);
+        if (rule != null && rule.getRestriction().get().permittedNumSec > 0) {
+            double usedToday = ActivityTime.getActivityTimeFromList(rule.getTimes(), rule.getRestriction().get().state);
 
-            if (usedToday > permittedNumSec) {
+            if (usedToday > rule.getRestriction().get().permittedNumSec) {
+                ;
                 long currentTime = Timestamp.valueOf(LocalDateTime.now()).getTime();
                 //notify or call method every numOfSecBetweenNotifications seconds after time is exceeded
-                if (currentTime > lastNotification + (1000 * numOfSecBetweenNotifications)) {
-                    lastNotification = currentTime;
-                    //TODO Notify user
-                    System.out.println("Time used up for " + rule.getExePath() + ", " + action);
+                if ( !isDisplayed &&
+                        currentTime > rule.getRestriction().get().lastNotification + (1000 * rule.getRestriction().get().numOfSecBetweenNotifications)) {
+
+                    rule.getRestriction().get().lastNotification = currentTime;
+                    this.isDisplayed = true;
+                    showAlert(rule);
+                    this.isDisplayed = false;
                 }
             }
         }
     }
+
+    public static void showAlert(Rule rule){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("WARNING");
+        alert.setContentText("Time on rule " + rule.toString() + " exceeded");
+        alert.showAndWait();
+    }
+
 
     public ExceededUsageAction getAction() {
         return action;
